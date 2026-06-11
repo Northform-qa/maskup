5add8751d260fa5891caccc9e62dc3cf79ce155f
Luke Hahn <Luke.hahn676@gmail.com>
2026-06-11 17:17:09 -0400

    try 60px

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,7 +33,7 @@ export default function NavBar() {
     <nav className="hidden md:flex items-center justify-between px-6 py-3 bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="flex items-center gap-8">
         <Link to="/" className="flex items-center">
-          <img src={compactLockup} alt="MaskUp.gg" className="h-14 w-auto" />
+          <img src={compactLockup} alt="MaskUp.gg" className="h-[60px] w-auto" />
         </Link>
         <div className="flex items-center gap-6">
           {NAV_LINKS.map((link) => (
